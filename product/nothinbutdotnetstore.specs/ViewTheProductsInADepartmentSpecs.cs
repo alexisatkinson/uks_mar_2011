2f636a67589a6935fd530b367c20171db9fb413e
Alexis Atkinson <mail@alexisatkinson.co.uk>
2011-03-16 17:00:33 +0000

    Pushing new changes

--- product/nothinbutdotnetstore.specs/ViewTheProductsInADepartmentSpecs.cs
+++ product/nothinbutdotnetstore.specs/ViewTheProductsInADepartmentSpecs.cs
@@ -1,6 +1,10 @@
+using System.Collections.Generic;
+using developwithpassion.specifications.extensions;
 using developwithpassion.specifications.rhino;
 using Machine.Specifications;
+using nothinbutdotnetstore.web.application;
 using nothinbutdotnetstore.web.application.catalogbrowsing;
+using nothinbutdotnetstore.web.application.model;
 using nothinbutdotnetstore.web.core;
 
 namespace nothinbutdotnetstore.specs
@@ -15,8 +19,33 @@ namespace nothinbutdotnetstore.specs
         [Subject(typeof(ViewTheProductsInADepartment))]
         public class when_run : concern
         {
-            It first_observation = () => { };
+            Establish e = () =>
+            {
+                request = an<Request>();
+                rendering_gateway = the_dependency<RenderingGateway>();
+                product_repository = the_dependency<ProductRepository>();
 
+                the_list_of_products = new List<Product> { new Product() };
+                the_department = new Department();
+
+                request.setup(x => x.map<Department>()).Return(the_department);
+
+                product_repository.setup(x => x.get_the_products_for(the_department)).
+                    Return(the_list_of_products);
+            };
+
+            Because b = () =>
+                sut.process(request);
+
+
+            It should_tell_the_rendering_gateway_to_display_the_products_for_a_department = () =>
+                rendering_gateway.received(x => x.render(the_list_of_products));
+
+            static Request request;
+            static ProductRepository product_repository;
+            static IEnumerable<Product> the_list_of_products;
+            static RenderingGateway rendering_gateway;
+            static Department the_department;
         }
     }
 }
---
+++ product/nothinbutdotnetstore/web/application/stubs/StubProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using nothinbutdotnetstore.web.application.model;
+
+namespace nothinbutdotnetstore.web.application.stubs
+{
+    public class StubProductRepository : ProductRepository
+    {
+        public IEnumerable<Product> get_the_products_for(Department department)
+        {
+            return Enumerable.Range(0, 10).Select(x => new Product());
+        }
+    }
+}
