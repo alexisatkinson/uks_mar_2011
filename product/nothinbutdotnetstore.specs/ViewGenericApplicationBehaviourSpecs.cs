9e0dd3d38991f18735bf19d3aaadc4d75fb04096
Alexis Atkinson <mail@alexisatkinson.co.uk>
2011-03-17 11:26:51 +0000

    Pushing new changes

--- product/nothinbutdotnetstore.specs/ViewGenericApplicationBehaviourSpecs.cs
+++ product/nothinbutdotnetstore.specs/ViewGenericApplicationBehaviourSpecs.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using developwithpassion.specifications.extensions;
 using developwithpassion.specifications.rhino;
 using Machine.Specifications;
-using nothinbutdotnetstore.web.application;
 using nothinbutdotnetstore.web.application.catalogbrowsing;
 using nothinbutdotnetstore.web.core;
 
@@ -22,13 +21,11 @@ namespace nothinbutdotnetstore.specs
             {
                 all_numbers = Enumerable.Range(1, 100).Select(x => new SomeType()).ToList();
                 renderer = the_dependency<RenderingGateway>();
-                the_catalog = the_dependency<StoreCatalog>();
                 the_request = an<Request>();
 
-                provide_a_basic_sut_constructor_argument<ViewRepositoryQuery<IEnumerable<SomeType>>>((x, y) =>
+                provide_a_basic_sut_constructor_argument<ViewRepositoryQuery<IEnumerable<SomeType>>>(x =>
                 {
-                    x.ShouldEqual(the_catalog);
-                    y.ShouldEqual(the_request);
+                    x.ShouldEqual(the_request);
                     return all_numbers;
                 });
             };
@@ -39,11 +36,8 @@ namespace nothinbutdotnetstore.specs
             It should_tell_the_renderer_to_display_the_information_retrieved_by_the_query = () =>
                 renderer.received(x => x.render(all_numbers));
 
-            static ViewRepositoryQuery<IEnumerable<SomeType>> query;
             static RenderingGateway renderer;
-            static Request request;
             static IEnumerable<SomeType> all_numbers;
-            static StoreCatalog the_catalog;
             static Request the_request;
         }
 
--- product/nothinbutdotnetstore/web/core/stubs/StubSetOfCommands.cs
+++ product/nothinbutdotnetstore/web/core/stubs/StubSetOfCommands.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using nothinbutdotnetstore.web.application.catalogbrowsing;
 using nothinbutdotnetstore.web.application.model;
+using nothinbutdotnetstore.web.application.stubs;
 
 namespace nothinbutdotnetstore.web.core.stubs
 {
@@ -15,11 +16,11 @@ namespace nothinbutdotnetstore.web.core.stubs
         public IEnumerator<RequestCommand> GetEnumerator()
         {
             yield return new DefaultRequestCommand(x => true,
-                                                   new ViewAReportModel<IEnumerable<Department>>((x,y) => x.get_the_main_departments()));
+                                                   new ViewAReportModel<IEnumerable<Department>>(x => Stub.with<StubStoreCatalog>().get_the_main_departments()));
             yield return new DefaultRequestCommand(x => true,
-                                                   new ViewAReportModel<IEnumerable<Product>>((x,y) => x.get_the_products_in(y.map<Department>())));
+                                                   new ViewAReportModel<IEnumerable<Product>>((x => Stub.with<StubStoreCatalog>().get_the_products_in(x.map<Department>()))));
             yield return new DefaultRequestCommand(x => true,
-                                                   new ViewAReportModel<IEnumerable<Department>>((x,y) => x.get_the_departments_in(y.map<Department>())));
+                                                   new ViewAReportModel<IEnumerable<Department>>((x => Stub.with<StubStoreCatalog>().get_the_departments_in(x.map<Department>()))));
         }
     }
 }
