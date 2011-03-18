2cbb867a475b1d275da53c0d309bee03547e09fc
Alexis Atkinson <mail@alexisatkinson.co.uk>
2011-03-18 21:57:40 +0000

    Pushing new changes

--- product/nothinbutdotnetstore/utility/containers/basic/BasicDependencyContainer.cs
+++ product/nothinbutdotnetstore/utility/containers/basic/BasicDependencyContainer.cs
@@ -4,10 +4,16 @@ namespace nothinbutdotnetstore.utility.containers.basic
 {
     public class BasicDependencyContainer : DependencyContainer
     {
+        Dependencies dependencies;
+
+        public BasicDependencyContainer(Dependencies dependencies)
+        {
+            this.dependencies = dependencies;
+        }
 
         public Dependency an<Dependency>()
         {
-            throw new NotImplementedException();
+            return (Dependency) dependencies.get_factory_that_can_create(typeof(Dependency)).create();
         }
     }
 }
